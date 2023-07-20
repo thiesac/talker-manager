85a82109d938e2cbcdaef1e6f67b15b224af0429
Thiesa <thiesa.c@gmail.com>
2023-07-20 15:42:32 -0400

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+// const HTTP_BAD_REQUEST = 400;
+const HTTP_NOT_FOUND = 404;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -16,6 +18,16 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (req, res) => {
   const file = await readFile();
   res.status(HTTP_OK_STATUS).json(file);
+});
+
+app.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const file = await readFile();
+    const found = file.find((person) => person.id === Number(id));
+  if (found) {
+    return res.status(HTTP_OK_STATUS).json(found); 
+  } 
+    return res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.listen(PORT, () => {
